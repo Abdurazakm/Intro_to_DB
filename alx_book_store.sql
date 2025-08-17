67b6187b3ecdf222d1925d932aa9449f554cc6d7
abdurazakm <abdulkem5472gmail.com>
2025-08-17 21:30:39 +0300

    modified

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,10 +1,16 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
 CREATE TABLE
-    Books (
-        book_id INT AUTO_INCREMENT PRIMARY KEY,
+    IF NOT EXISTS Authors (
+        author_id INT PRIMARY KEY,
+        author_name VARCHAR(215)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS Books (
+        book_id INT PRIMARY KEY,
         title VARCHAR(130),
         author_id INT,
         price DOUBLE,
@@ -13,30 +19,24 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    Authors (
-        author_id INT AUTO_INCREMENT PRIMARY KEY,
-        author_name VARCHAR(215)
-    );
-
-CREATE TABLE
-    Customers (
-        customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    IF NOT EXISTS Customers (
+        customer_id INT PRIMARY KEY,
         customer_name VARCHAR(215),
-        email VARCHAR(215) UNIQUE,
+        email VARCHAR(215),
         address TEXT
     );
 
 CREATE TABLE
-    Orders (
-        order_id INT AUTO_INCREMENT PRIMARY KEY,
+    IF NOT EXISTS Orders (
+        order_id INT PRIMARY KEY,
         customer_id INT,
         order_date DATE,
         FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
     );
 
 CREATE TABLE
-    Order_Details (
-        orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    IF NOT EXISTS Order_Details (
+        orderdetailid INT PRIMARY KEY,
         order_id INT,
         book_id INT,
         quantity DOUBLE,
